5639300fd2a64103a2014650c1302fbd63f4ae0c
AlpsiChillikas <yazstakarbo@gmail.com>
2025-02-07 21:24:17 +0200

    Update nuppud.js

--- nuppud.js
+++ nuppud.js
@@ -2,9 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const buttons = document.querySelectorAll("button");
     const urls = ["/alpsichillikad/team", "/alpsichillikad/kalender", "/alpsichillikad/invite", "/alpsichillikad/esport", "/alpsichillikad/meiesaavutused"];
 
-    buttons.forEach((button, index) => {
-        button.addEventListener("click", function () {
-            window.location.href = urls[index];
-        });
+    button.addEventListener("click", function () {
+        window.location.href = window.location.origin + urls[index];
     });
 });
